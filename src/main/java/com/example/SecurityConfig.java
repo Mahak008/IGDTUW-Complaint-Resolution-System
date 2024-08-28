2ae88d611f26005680dc7476b69e1cb8b424cf1b
Mahak Garg <mahakgarg8002@gmail.com>
2024-08-28 13:36:45 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/SecurityConfig.java
+++ src/main/java/com/example/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers("/", "/admin/admin_login", "/student/login", "/student/register", "/css/**")
+                        .requestMatchers("/", "/admin/admin_login", "/student/login", "/student/register", "/css/**", "/images/**/", "/user_details/**")
                         .permitAll()
                         .requestMatchers("/student/student", "/complaint").authenticated()
                         .anyRequest().authenticated())
